4065e08c72374da117b3061252ce4ac6a0744cd4
Json_Lee <2622336659@qq.com>
2024-08-21 01:31:51 +0800

    feat: 新增环境变量解析

--- apps/jsonlee-admin/vite.config.ts
+++ apps/jsonlee-admin/vite.config.ts
@@ -2,6 +2,22 @@ import { type ConfigEnv, defineConfig, loadEnv, ProxyOptions, type UserConfig } 
 import react from '@vitejs/plugin-react';
 
 type ProxyItem = [string, string];
+const parseConfig = (env: Record<string, string>): Record<string, any> => {
+  const res: Record<string, any> = {};
+  for (const key in env) {
+    if (env.hasOwnProperty(key)) {
+      const item = env[key];
+      const jsonStr = item.replace(/'/g, '"');
+      try{
+        const json = JSON.parse(jsonStr);
+        res[key] = json;
+      }catch (e) {
+        res[key] = item;
+      }
+    }
+  }
+  return res;
+};
 const createProxy = (proxies: ProxyItem[]): Record<string, ProxyOptions> => {
   if (!proxies) return {};
   const res: Record<string, ProxyOptions> = {};
@@ -13,14 +29,15 @@ const createProxy = (proxies: ProxyItem[]): Record<string, ProxyOptions> => {
       changeOrigin: true,
       ws: true,
       rewrite: (path) => path.replace(new RegExp(`^${p}`), ''),
-      ...(isHttps ? { secure: false } : {})
-    }
+      ...(isHttps ? { secure: false } : {}),
+    };
   }
   return res;
-}
+};
 // https://vitejs.dev/config/
-export default defineConfig(({mode}: ConfigEnv): UserConfig => {
-  const env = loadEnv(mode, process.cwd())
+export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
+  const env = loadEnv(mode, process.cwd());
+  const config = parseConfig(env);
   return {
     plugins: [react({
       babel: {
@@ -34,7 +51,7 @@ export default defineConfig(({mode}: ConfigEnv): UserConfig => {
       },
     },
     server: {
-      proxy: createProxy(JSON.parse(env['VITE_PROXY'] || '[]')),
+      proxy: createProxy(config['VITE_PROXY'] || []),
     },
-  }
+  };
 });
