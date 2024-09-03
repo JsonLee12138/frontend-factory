f1b57bb9a556790b5610a2b47e28d77c588a8296
Json_Lee <2622336659@qq.com>
2024-09-03 17:42:36 +0800

    docs: 新增arrTransform的文档注释

--- apps/jsonlee-admin/src/utils/arr.ts
+++ apps/jsonlee-admin/src/utils/arr.ts
@@ -1,16 +1,43 @@
 import { get } from 'lodash';
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type AnyObject = Record<string | number | symbol, any>;
+
 type KeyConfig = {
   source: string;
   target: string;
 };
 
-export const arrTransform = <
-  S extends Record<string, any>,
-  T extends Record<string, any>,
->(
+/**
+ * Transforms an array of objects from one format to another based on a provided mapping.
+ * 根据提供的映射，将一个对象数组从一种格式转换为另一种格式。
+ *
+ * @template S - The type of the source objects in the input array.
+ * @template S - 输入数组中源对象的类型。
+ *
+ * @template T - The type of the transformed objects in the output array.
+ * @template T - 输出数组中转换后的对象类型。
+ *
+ * @param {S[]} data - The array of source objects to be transformed.
+ * @param {S[]} data - 要转换的源对象数组。
+ *
+ * @param {AnyObject} format - The mapping object that defines the key transformation from source to target.
+ * The keys represent the target object's keys, and the values represent the corresponding keys in the source object.
+ * @param {AnyObject} format - 定义从源到目标的键转换的映射对象。
+ * 键表示目标对象的键名，值表示源对象中对应的键名。
+ *
+ * @param {boolean} [deep=false] - Whether to perform deep transformation on nested objects specified by the childrenKey.
+ * @param {boolean} [deep=false] - 是否对由 childrenKey 指定的嵌套对象执行深度转换。
+ *
+ * @param {KeyConfig} [childrenKey={source: 'children', target: 'children'}] - The key configuration for handling nested objects.
+ * @param {KeyConfig} [childrenKey={source: 'children', target: 'children'}] - 用于处理嵌套对象的键配置。
+ *
+ * @returns {T[]} - The transformed array of objects in the new format.
+ * @returns {T[]} - 转换后的新格式的对象数组。
+ */
+export const arrTransform = <S extends AnyObject, T extends AnyObject>(
   data: S[],
-  format: Record<string, any>,
+  format: AnyObject,
   deep?: boolean,
   childrenKey: KeyConfig = {
     source: 'children',
@@ -18,7 +45,7 @@ export const arrTransform = <
   },
 ): T[] => {
   return data.map((item) => {
-    const resItem: Record<string, any> = {};
+    const resItem: AnyObject = {};
     for (const targetKey in format) {
       if (Object.prototype.hasOwnProperty.call(format, targetKey)) {
         const sourceKey = format[targetKey];
